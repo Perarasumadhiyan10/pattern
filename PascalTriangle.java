6a9a36d782eedd42312145fc10073c4a16449542
ZBOOK <perarasuperarasu892@gmail.com>
2026-08-14 12:18:46 +0530

    Change the pascaltriangle

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,22 +1,27 @@
 import java.util.*;
-
 public class PascalTriangle {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        System.out.println("** Printing the pattern... **");
-
-        for (int i = 0; i < n; i++) {
-            int val = 1;
-            for(int k=0;k<n-i-1;k++){
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= i; j++) {
-                System.out.print(val+" ");
-                val = val * (i - j) / (j + 1);
-            }
-            System.out.println();
+      Scanner sc = new Scanner(System.in);
+      int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+          for(int k=0;k<n-i;k++){
+            System.out.print(" ");
+          } 
+          int a=1;
+          for(int j=0;j<=i;j++){
+            System.out.print(a+" ");
+            a = a*(i-j)/(j+1);
+          }
+          System.out.println();
         }
     }
 }
+
+// Input : 5
+// output : 
+    //  1 
+    // 1 1 
+//    1 2 1 
+//   1 3 3 1 
+//  1 4 6 4 1 
+// 1 5 10 10 5 1 
